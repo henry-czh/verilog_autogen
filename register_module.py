10288ba52cace592b7507b00330f24448c23dd65
henry-czh <chaozhanghu123@qq.com>
2019-02-17 00:24:00 +0800

    v1.1
    add main logic for apb interface

--- register_module.py
+++ register_module.py
@@ -33,19 +33,73 @@ module %s(
     s=s % (module_name)
     output_file.write(s)
 
-def gen_wr(output_file):
+def gen_main(output_file,reg_info):
     s = '''
-wire pready;
-wire [31:0] prdata;
+reg pready;
+reg [31:0] prdata;
 
-assign pready=1;
 assign reg_wr=penable & psel & pwrite;
 assign reg_rd=penable & psel & ~pwrite;
 
-always@(posedege clk)
+always@(posedge clk)
 if(~rst_n)
-    p
+    pready <= 1'b0;
+else if(penable & psel)
+    pready <= 1'b1;
+else
+    pready <= 1'b0;
+
 '''
+    output_file.write(s)
+
+    for data in reg_info:
+        output_file.write('reg [31:0] %s;\n' % (data[0]))
+    
+    s='''
+always@(posedge clk)
+if(~rst_n) begin
+'''
+    output_file.write(s)
+    
+    for data in reg_info:
+        if data[3].find('W'):
+            output_file.write('\t%s <= \'h%s;\n' % (data[0],data[2].split('x')[1]))
+    
+    s='''end
+else if(reg_wr) begin
+    case(paddr)
+'''
+    output_file.write(s)
+    
+    for data in reg_info:
+        if data[3].find('W'):
+            output_file.write('\t32\'h%s: %s <= pwdata;\n' % (data[1].split('x')[1],data[0]))
+    s='''    default:
+    endcase
+end 
+'''
+    output_file.write(s)
+    
+    s='''
+always@(posedge clk)
+if(~rst_n)
+    prdata <= 'h0;
+else if(reg_rd) begin
+    case(paddr)
+'''
+    output_file.write(s)
+    
+    for data in reg_info:
+        if data[3].find('R'):
+            output_file.write('\t32\'h%s: prdata <= %s;\n' % (data[1].split('x')[1],data[0]))
+    s='''    default:
+    endcase
+end 
+
+endmodule
+'''
+    output_file.write(s)
+
 def deal_xls(sheet):
     book=xlrd.open_workbook(sys.argv[1])
     page=book.sheet_by_index(sheet)
@@ -64,6 +118,8 @@ def deal_xls(sheet):
 if __name__=='__main__':
     reg_info=deal_xls(0)[0]
     module_info=deal_xls(0)[1]
+    #print reg_info
     output_file=open('%s.v' % module_info[0],'w')
     gen_header(output_file,module_info[0])
+    gen_main(output_file,reg_info)
     output_file.close()
